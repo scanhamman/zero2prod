aa173bbed88727faa295ce5d83ce2ae7762d34cb
Steve Canham <stevecanham@outlook.com>
2024-11-12 15:47:21 +0000

    initial framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+   match run().await {
+      Ok(_s) => Ok(()),
+      Err(e) => Err(e),
+   }
 }
+ 
